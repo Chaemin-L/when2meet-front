d93a479b060bac4725f58c8cda9e1e2d45a4e9a0
Chaemin-L <chaem12@kookmin.ac.kr>
2022-08-15 03:46:33 +0900

    refactor: Make all Routers being in App component

--- temp_design/src/App.js
+++ temp_design/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import NavigationBar from './NavigationBar';
-import Content from './Content';
+import { Routes, Route } from 'react-router-dom';
+import { NavBar, NavItem } from './components/index';
+import {Introduction, Create, Schedule} from './pages/index';
 
 
 function App() {
   return (
     <>
-      <NavigationBar />
-      <Content />
+      <NavBar>
+            <NavItem to="/about" >About when2meet</NavItem>
+            <NavItem to="/">Plan a new Event</NavItem>
+      </NavBar>
+      <Routes>
+            <Route path="/" element={<Create />} />
+            <Route path="/about" element={<Introduction />} />
+            <Route path="/schedule" element={<Schedule />} />
+      </Routes>
     </>
   );
 };
---
+++ temp_design/src/pages/index.js
@@ -0,0 +1,3 @@
+export {default as Create } from './Create';
+export {default as Introduction } from './Introduction';
+export {default as Schedule } from './Schedule';
