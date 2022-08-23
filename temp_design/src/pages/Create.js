9e0d90085b0a945ab7c57f6cb48a8aa5e545a0b7
Chaemin-L <chaem12@kookmin.ac.kr>
2022-08-24 04:57:07 +0900

    feat: Implement date-range-picker

--- temp_design/src/pages/Create.js
+++ temp_design/src/pages/Create.js
@@ -1,13 +1,19 @@
 import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Wrapper, Input, Button } from '../components/index';
+import "react-modern-calendar-datepicker/lib/DatePicker.css";
+import { Calendar, utils } from "@amir04lm26/react-modern-calendar-date-picker";
 
 export default function Create() {
     const [input, setInput] = useState();
+    const [selectedDayRange, setSelectedDayRange] = useState({
+        from: null,
+        to: null
+      });
 
     const navigate = useNavigate();
     const onChange = (e) => {
-        setInput(e.target.value);
+            setInput(e.target.value);
     };
     
     const onClick = () => {
@@ -15,6 +21,7 @@ export default function Create() {
             alert('올바른 일정 이름을 입력해주세요.');
             return;
         }
+        // API to create new DB 
         navigate('/schedule', {
             state: {
                 eventName: input,
@@ -23,13 +30,19 @@ export default function Create() {
     };
     
     return (
+
         <Wrapper>
             <h1>Main</h1>
-            <Input placeholder="새로운 일정 이름" onChange={onChange} />
-            <p>
-                날짜 선택 창이 들어갈 자리입니다.
-            </p>
-            <Button onClick={onClick}>일정 생성하기</Button>
-        </Wrapper>
+            <Input placeholder="새로운 일정 이름" onChange={onChange} />  
+            
+            <Calendar
+            value={selectedDayRange}
+            onChange={setSelectedDayRange}
+            minimumDate={utils().getToday()}
+            shouldHighlightWeekends
+            />
+                <Button onClick={onClick}>일정 생성하기</Button>
+            </Wrapper>
+        
     );
 };
