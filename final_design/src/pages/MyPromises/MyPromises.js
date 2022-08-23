56d7f8441146adf6a43e7ec347dd706fbf43510a
Chaemin-L <chaem12@kookmin.ac.kr>
2022-08-24 04:41:51 +0900

    test: Move test DB to new file

--- final_design/src/pages/MyPromises/MyPromises.js
+++ final_design/src/pages/MyPromises/MyPromises.js
@@ -1,33 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Promises from './Promises';
-
-const temp = [
-    {
-        id: 0,
-        name: '약속',
-        startDay: '2022-08-11',
-        endDay: '2022-08-30',
-        place: '신촌',
-        text: '지민, 찬호 오기로 함'
-    },
-    {
-        id: 1,
-        name: '동창회',
-        startDay: '2022-08-27',
-        endDay: '2022-09-03',
-        place: '종로',
-        text: '',
-    },
-
-]
+import {promises} from 'testDB'
 
 export default function MyPromises() {
     
     return (
         <div>
             <Header />
-            <Promises promises={temp} />
+            <Promises promises={promises} />
         </div>
     )
 }
---
+++ final_design/src/testDB.js
@@ -0,0 +1,19 @@
+export const promises = [
+    {
+        id: 0,
+        name: '약속',
+        startDay: '2022-08-11',
+        endDay: '2022-08-30',
+        place: '신촌',
+        text: '지민, 찬호 오기로 함'
+    },
+    {
+        id: 1,
+        name: '동창회',
+        startDay: '2022-08-27',
+        endDay: '2022-09-03',
+        place: '종로',
+        text: '',
+    },
+
+];
