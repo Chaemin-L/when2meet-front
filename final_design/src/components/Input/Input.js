573565ebdce3719b2ea8a481a37856003568b0a8
Chaemin-L <chaem12@kookmin.ac.kr>
2022-09-02 22:33:09 +0900

    Add placeholder and defaultValue to input

--- final_design/src/components/Input/Input.js
+++ final_design/src/components/Input/Input.js
@@ -3,29 +3,29 @@ import { StyledInput, StyledLabel } from './StyledInput';
 import visibility from 'assets/img/visibility.png';
 import visibilityOff from 'assets/img/visibilityOff.png';
 
-export default function Input({ header, option, onChange, isPassword }) {
-    	// password visibility 제어
-        const [passwordType, setPasswordType] = useState({
-            type: 'password',
-            visible: false
-        });
+export default function Input({ header, option, onChange, isPassword=false, placeholder='' }) {
+    // password visibility 제어
+    const [passwordType, setPasswordType] = useState({
+        type: 'text',
+        visible: !isPassword,
+    });
         
         //password 제어 핸들러
-        const handlePasswordType = e => {
-            setPasswordType(() => {
-                if (!passwordType.visible) {
-                    e.target.src = visibility;
-                    return { type: 'text', visible: true };
-                }
-                e.target.src = visibilityOff;
-                return { type: 'password', visible: false };
-            })
-        }
+    const handlePasswordType = e => {
+        setPasswordType(() => {
+            if (!passwordType.visible) {
+                e.target.src = visibility;
+                return { type: 'text', visible: true };
+            }
+            e.target.src = visibilityOff;
+            return { type: 'password', visible: false };
+        })
+    };
     return (
         <div>
             <StyledLabel option={option}> {header}
-                <StyledInput onChange={onChange} option={option} {...passwordType} />
-                {isPassword && <img src={visibilityOff} onClick={handlePasswordType} alt='visibilityProps' />}
+                <StyledInput onChange={onChange} option={option} placeholder={placeholder} {...passwordType} />
+                {isPassword && <img src={visibility} onClick={handlePasswordType} alt='visibilityProps' />}
             </StyledLabel>
         </div>
     )
